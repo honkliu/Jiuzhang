02c4f658511c6dac59060d0a5d21f0ce7fe55dac
Ubuntu <azureuser@shug3y75a000000.jscfurjom2sunpff2aidglfcsa.gx.internal.cloudapp.net>
2025-07-03 23:04:43 +0000

    amd

--- scripts/start-amd.sh
+++ scripts/start-amd.sh
@@ -9,12 +9,12 @@ docker run \
   --ipc=host \
   --name dskf \
   -p 30000:30000 \
-  -v /mnt/resource_nvme:/mnt/resource_nvme \
-  -e HF_HOME=/mnt/resource_nvme/hf_cache \
+  -v /mnt/nvme_raid0:/mnt/nvme_raid0 \
+  -e HF_HOME=/mnt/nvme_raid0/hf_cache \
   -e HSA_NO_SCRATCH_RECLAIM=1 \
   -e GPU_FORCE_BLIT_COPY_SIZE=64 \
   -e DEBUG_HIP_BLOCK_SYN=1024 \
   rocm/sgl-dev:20250701 \
-  python3 -m sglang.launch_server --model-path /mnt/resource_nvme/DeepSeek-R1-0528 --tp 8 --trust-remote-code --chunked-prefill-size 131072 --enable-torch-compile --torch-compile-max-bs 256 --host 0.0.0.0 --port 30000 --api-key=123 > /mnt/resource_nvme/gitroot/output.log 2>&1
+  python3 -m sglang.launch_server --model-path /mnt/resource_nvme/DeepSeek-R1-0528 --tp 8 --trust-remote-code --chunked-prefill-size 131072 --torch-compile-max-bs 256 --host 0.0.0.0 --port 30000 > /mnt/nvme_raid0/gitroot/output.log 2>&1
 
 
